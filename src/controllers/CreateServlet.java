778dce7fa3f74bdb3b2ea676f2704566d8d7bc66
chie <chie@localhost>
2022-01-30 17:57:52 +0900

    フラッシュメッセージを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -48,9 +48,12 @@ public class CreateServlet extends HttpServlet {
             t.setCreated_at(currentTime);
             t.setUpdated_at(currentTime);
 
+            // データベースに保存
             em.persist(t);
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
             em.close();
+
 
             response.sendRedirect(request.getContextPath() + "/index");
         }
